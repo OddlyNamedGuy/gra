0d5a4e3e276cc80db11564b2114780d6066e428f
OddlyNamedGuy <61271975+OddlyNamedGuy@users.noreply.github.com>
2020-02-20 13:17:45 +0100

    o i x Alex

---
+++ gra.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+
+class Game:
+
+    def __init__(self, name, board, player1, player2):
+        self.name = board
+        self.board = name
+        self.player1 = player1
+        self.player2 = player2
+        
+    runda = 0
+    while True:
+        for i in range(2):
+            if i == 0:
+                runda = 0
+
+            if i == 1:
+                runda = 1
+
+
+
+
+
+
+
+    def wybor(self, choice, miejsce):
+        if choice != 0 or 1
+
+
+    numpy.diagonal
+    numpy.reshape
+
+
+class Uzytkownik:
+    def __int__(self, name):
+        self.name = name
+
+
+
+
+class Board:
+    def __init__(self):
+        plansza = [[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]
+        self.plansza = np.array(plansza)
+
+
+
+
+
